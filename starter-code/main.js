1afd2419c4289ca197c829a32aefa7199adcd62a
Paris Leach <parisleach@Pariss-MacBook-Pro.local>
2016-09-18 23:30:31 -0400

    finished game and updated game logic and images

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,15 +5,45 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
 var createBoard = function(){
 	var gameBoard = document.getElementById('game-board');
-	for(var i = 0; i < 4; i++) {
+	for(var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
 		newCard.className = 'card';
+		newCard.setAttribute('data-card', cards[i]);
+		newCard.addEventListener('click', isTwoCards)
 		gameBoard.appendChild(newCard);
 	}
 }
+
+function isMatch(cards) {
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }
+}
+
+
+var isTwoCards = function() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png'>"; 
+	} else {
+		this.innerHTML = "<img src='queen.png'>";
+	}
+  
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
 createBoard();
+
 
 /*
 if (cardOne === cardTwo) {
