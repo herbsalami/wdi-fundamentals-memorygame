5ed13ba7d505aa4c50950591431a9de0cff13249
Paris Leach <parisleach@Pariss-MacBook-Pro.local>
2016-09-18 22:30:41 -0400

    transferred game-board creation to javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,56 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+var createBoard = function(){
+	var gameBoard = document.getElementById('game-board');
+	for(var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		gameBoard.appendChild(newCard);
+	}
+}
+createBoard();
+
+/*
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardOne === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardThree === cardOne) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
+
+if (cardThree === cardTwo) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+} */
